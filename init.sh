3107674ee6ce35bb2c13e9c3d59d2da39fa4fdd0
Armaan Tobaccowalla <13340433+ArmaanT@users.noreply.github.com>
2020-01-13 21:58:27 -0500

    Merge backend and frontend domains

--- init.sh
+++ init.sh
@@ -15,10 +15,16 @@ mkdir frontend
 
 django-admin startproject --template=https://github.com/pennlabs/template-django/archive/master.zip --extension=py,cfg,yaml --name=Dockerfile $1 backend
 
+# Remove unnecessary backend files
 rm -r backend/circleci
 rm -rf backend/k8s
 rm backend/.gitignore
 rm backend/LICENSE
 rm backend/README.md
+
+# Prefix backend routes with api/
+sed -i "s/\/static\//\/api\/static\//g" backend/$1/settings/base.py
+sed -i "/^if settings.DEBUG:/i urlpatterns = \[path(\"api\/\", include(urlpatterns))\]\n" backend/$1/urls.py
+sed -i "s/\/static=/\/api\/static=/g" backend/setup.cfg
 
 rm init.sh
